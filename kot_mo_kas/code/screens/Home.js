796b42545e95115d61763c957b5fd55c4e829a3b
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-05-18 14:40:18 +0400

    Home page done

--- kot_mo_kas/code/screens/Home.js
+++ kot_mo_kas/code/screens/Home.js
@@ -1,13 +1,42 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { View, Text, Image, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import styles from '../styles/Styles';
+// import budget from '../../assets/budget_pic.jpeg';
 
 export default function HomeScreen({navigation}) {
+
+    const onPress = () => navigation.navigate('Add');
+
     return(
         <View style={styles.container}>
-            <Text>Home</Text>
+            <ScrollView>
+                {/* Top */}
+                <View style={styles.top_home}>
+                    <Text style={styles.home_title}>
+                        Welcome to{"\n"}
+                        Kot mo kas
+                    </Text>
+                    <View style={styles.hr_home}></View>
+                    <Text style={styles.home_intro}>
+                        Are you having trouble to keep track of your monthly spending? Are you searching for a centralized place to keep track of your expenses? Well, you are at the right place!
+                    </Text>
+                </View>
+                {/* Middle */}
+                <View style={styles.home_pic_container}>
+                    <TouchableOpacity
+                        style={styles.home_btn}
+                        onPress={onPress}>
+                            <Text style={{color: '#ffffff'}}>Get Started</Text>
+                    </TouchableOpacity>
+                    <Image source={require('../../assets/budget_pic.jpeg')} style={{width: 170, height: 170}}/>
+                </View>
+                {/* Bottom */}
+                <View style={styles.home_bottom}>
+                    <Text style={{fontSize: 16, textAlign: 'center', paddingBottom: 10, fontStyle: 'italic'}}>"A budget is telling your money where to go instead of wondering where it went."{"\n"} - Dave Ramsey</Text>
+                </View>
+            </ScrollView>
         </View>
     );
 }
--- kot_mo_kas/code/styles/Styles.js
+++ kot_mo_kas/code/styles/Styles.js
@@ -2,21 +2,63 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        backgroundColor: '#fff',
+        padding: 23
     },
+    // Navigation Bar Shadow
     shadow: {
         shadowColor: '#595959',
         shadowOffset: {
             width: 0,
-            height: 10,
+            height: 10
         },
         shadowOpacity: 0.25,
         shadowRadius: 3.5,
         elevation: 5
-    }
+    },
+    // Home
+    top_home: {
+        display: 'flex',
+        flexDirection: "column"
+    },
+    home_title: {
+        textAlign: 'center',
+        fontSize: 28
+    },
+    hr_home: {
+        borderBottomColor: '#35B736',
+        borderBottomWidth: 1,
+        width: 150,
+        marginLeft:'auto',
+        marginRight: 'auto',
+        marginBottom: 25
+    },
+    home_intro: {
+        textAlign:'center',
+        padding: 2,
+        marginBottom: 22,
+        fontSize: 16
+    },
+    home_pic_container: {
+        marginTop: 8,
+        marginBottom: 8,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    home_bottom: {
+        fontSize: 15,
+        marginTop: 15
+    },
+    home_btn: {
+        alignItems: 'center',
+        backgroundColor: '#00ce00',
+        padding: 15,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        borderRadius: 10
+    }  
 });
 
 export default styles;
