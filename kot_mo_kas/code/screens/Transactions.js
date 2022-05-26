40bf6a945a5c6eff70d46b39796f506c10f5d3f8
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-05-26 23:22:56 +0400

    samll fix

--- kot_mo_kas/code/screens/Transactions.js
+++ kot_mo_kas/code/screens/Transactions.js
@@ -7,13 +7,13 @@ export default function Transactions({navigation}) {
     return(
         <ScrollView style={styles.container}>
             {data.map(a => {
-                let amount = Number(a.amount) * -1
+                let amount = Number(a.amount)
                 if(amount < 0) {
                     return (
                     <View style={styles.expense_trans}>
                         <Text style={{fontSize: 16}}><Text style={{fontWeight: 'bold'}}>{a.category}</Text>{'\n'}{'\n'}{a.date}</Text>
                         <View style={{position: 'absolute', right: 15, bottom: 37}}>
-                            <Text style={{fontSize: 16, color: 'red'}}>Rs -{a.amount}</Text>
+                            <Text style={{fontSize: 16, color: 'red'}}>- Rs {a.amount * -1}</Text>
                         </View>
                     </View>
                     )
@@ -22,7 +22,7 @@ export default function Transactions({navigation}) {
                         <View style={styles.income_trans}>
                         <Text style={{fontSize: 16}}><Text style={{fontWeight: 'bold'}}>{a.category}</Text>{'\n'}{'\n'}{a.date}</Text>
                         <View style={{position: 'absolute', right: 15, bottom: 37}}>
-                            <Text style={{fontSize: 16, color: 'green'}}>+ Rs {(a.amount)* -1}</Text>
+                            <Text style={{fontSize: 16, color: 'green'}}>+ Rs {a.amount}</Text>
                         </View>
                     </View>
                     )
